dffeca7b75c5c71d4930770bacc4a112a73710c6
IDerdi <92827333+IDerdi@users.noreply.github.com>
2023-06-13 12:31:55 +0300

    wsdf

--- portfolio/app.js
+++ portfolio/app.js
@@ -2,6 +2,27 @@ let navLink=document.querySelector("#nav-link");
 let dropDown=document.querySelector(".dropdown1")
 let main=document.querySelector("main")
 
+let circularProgress=document.querySelector(".circular-progresss"),
+    progressValue=document.querySelector(".progress-value");
+
+let progressStartValue=0,
+    progressEndValue=90,
+    speed= 100;
+    bgColor=94e044
+
+let progress = setInterval(()=>{
+    progressStartValue++;
+    
+    progressValue.innerHTML=`${progressStartValue}%`
+    circularProgress.style.background=`conic-gradient(#94e044 ${progressStartValue * 3.6 }deg , #ededed 0deg)`
+
+    if(progressStartValue == progressEndValue ){
+        clearInterval(progress)
+    }
+
+}, speed)
+
+
 navLink.addEventListener("click" , function() {
     
    if (navLink.className=="") {
